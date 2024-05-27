21d58d3538215c558cb9404dc03dc7ff383f0278
Camobeast <jakub.lilley@gmail.com>
2024-05-27 19:00:05 +0200

    Added mailing php script and other changes

---
+++ send-email.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = htmlspecialchars($_POST['firstName']);
+    $lastName = htmlspecialchars($_POST['lastName']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $email = htmlspecialchars($_POST['email']);
+    $guests = htmlspecialchars($_POST['guests']);
+    $date = htmlspecialchars($_POST['date']);
+
+    $to = 'lilley@email.cz';
+    $subject = 'New Reservation Request';
+    $message = "You have a new reservation request:\n\n";
+    $message .= "Jméno: $firstName\n";
+    $message .= "Příjmení: $lastName\n";
+    $message .= "Telefon: $phone\n";
+    $message .= "Email: $email\n";
+    $message .= "Počet hostů: $guests\n";
+    $message .= "Termín: $date\n";
+
+    $headers = "From: noreply@yourdomain.com";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo 'Email sent successfully!';
+    } else {
+        echo 'Email sending failed.';
+    }
+} else {
+    echo 'Invalid request method.';
+}
+?>
